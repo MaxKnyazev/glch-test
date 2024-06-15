7eda9bc8a29a886a93c009d10553367ed451e2f4
MaxKnyazev <max.knyazev77@gmail.com>
2024-06-15 19:04:56 +0300

    style: changed variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,12 @@ const Header = () => {
 
   return (
     <header className='header'>
-      <h2>Header</h2>
+      <h2 className='header__title'>glch-test</h2>
+
+      <nav className="header__nav nav">
+        <span className="nav__item">1</span>
+        <span className="nav__item">1</span>
+      </nav>
     </header>
   )
 }
